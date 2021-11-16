68d0c651ed329df6e22913a97156e4159bca5fe4
Mike Neilens <mike.neilens@gmail.com>
2021-11-16 22:10:42 +0000

    initial advent2021

---
+++ day24/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.5.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
---
+++ day24/src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun `one equals one`() {
+        assertEquals(1,1)
+    }
+}
