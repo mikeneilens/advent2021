756c5652da9e8bd518e9264fdc504b6393a26ed8
Mike Neilens <mike.neilens@gmail.com>
2021-12-01 12:25:48 +0000

    day 1 minor refactor

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 
-data class AccumulatedResult (val previous:Int, val noOfIncreases:Int)
+data class AccumulatedResult (val previous:Int?, val noOfIncreases:Int)
 
 fun compareCurrentToPrevious(accumulatedResult:AccumulatedResult, current:Int):AccumulatedResult {
     val (previous, noOfIncreases) = accumulatedResult
-    return if (previous in 1 until current) AccumulatedResult(current, noOfIncreases + 1) else AccumulatedResult(current, noOfIncreases)
+    return if (previous != null && current > previous) AccumulatedResult(current, noOfIncreases + 1) else AccumulatedResult(current, noOfIncreases)
 }
 
-fun partOne(listOfDepths:List<Int>):Int =
-    listOfDepths.fold(AccumulatedResult(0, 0), ::compareCurrentToPrevious).noOfIncreases
+fun partOne(listOfDepths:List<Int>) =
+    listOfDepths.fold(AccumulatedResult(null, 0), ::compareCurrentToPrevious).noOfIncreases
 
 
-fun List<Int>.convertListToSlidingWindow():List<Int> = windowed(3,1).map{it.sum()}
+fun List<Int>.convertListToSlidingWindow() = windowed(3,1).map{it.sum()}
 
-fun partTwo(listOfDepths:List<Int>):Int = partOne(listOfDepths.convertListToSlidingWindow())
+fun partTwo(listOfDepths:List<Int>) = partOne(listOfDepths.convertListToSlidingWindow())
