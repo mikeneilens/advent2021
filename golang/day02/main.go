ba3a1010798bca2eaf460ef0d46faf978e845131
Mike Neilens <mike.neilens@gmail.com>
2021-12-02 17:55:14 +0000

    added day02 in golang

--- golang/day02/main.go
+++ golang/day02/main.go
@@ -11,47 +11,44 @@ type submarine struct {
 	aim int
 }
 
-func createListOfCommands(data string) []func(*submarine) {
+func createListOfCommands(data string, functionCreator func(commandText string, commands []func(*submarine), ndx int, qty int)) []func(*submarine) {
 	listOfWords := parser.ParseIntoListOfWords(data)
 	var result = make([]func(*submarine), len(listOfWords))
 
 	for ndx, record := range listOfWords {
 		command := record[0]
 		qty, _ := strconv.Atoi(record[1])
-		switch command {
-		case "forward":
-			result[ndx] = func(sub *submarine) { sub.x += qty }
-		case "up":
-			result[ndx] = func(sub *submarine) { sub.y -= qty }
-		case "down":
-			result[ndx] = func(sub *submarine) { sub.y += qty }
-		}
+		functionCreator(command, result, ndx, qty)
 	}
 	return result
 }
 
-func CreateListOfCommandsP2(data string) []func(*submarine) {
-	listOfWords := parser.ParseIntoListOfWords(data)
-	var result = make([]func(*submarine), len(listOfWords))
-	for ndx, record := range listOfWords {
-		command := record[0]
-		qty, _ := strconv.Atoi(record[1])
-		switch command {
-		case "forward":
-			result[ndx] = func(sub *submarine) { sub.x += qty; sub.y += sub.aim * qty }
-		case "up":
-			result[ndx] = func(sub *submarine) { sub.aim -= qty }
-		case "down":
-			result[ndx] = func(sub *submarine) { sub.aim += qty }
-		}
+func part1FunctionCreator(command string, listOfCommands []func(*submarine), ndx int, qty int) {
+	switch command {
+	case "forward":
+		listOfCommands[ndx] = func(sub *submarine) { sub.x += qty }
+	case "up":
+		listOfCommands[ndx] = func(sub *submarine) { sub.y -= qty }
+	case "down":
+		listOfCommands[ndx] = func(sub *submarine) { sub.y += qty }
 	}
-	return result
 }
 
-func day02(data string, listOfCommandsCreator func(string) []func(*submarine)) int {
+func part2FunctionCreator(command string, listOfCommands []func(*submarine), ndx int, qty int) {
+	switch command {
+	case "forward":
+		listOfCommands[ndx] = func(sub *submarine) { sub.x += qty; sub.y += sub.aim * qty }
+	case "up":
+		listOfCommands[ndx] = func(sub *submarine) { sub.aim -= qty }
+	case "down":
+		listOfCommands[ndx] = func(sub *submarine) { sub.aim += qty }
+	}
+}
+
+func day02(data string, functionCreator func(commandText string, commands []func(*submarine), ndx int, qty int)) int {
 	var sub = submarine{0, 0, 0}
 
-	commands := listOfCommandsCreator(data)
+	commands := createListOfCommands(data, functionCreator)
 
 	for _, command := range commands {
 		command(&sub)
