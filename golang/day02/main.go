94690313c96aead7e3554c1c8c9d509ed7fb0018
Mike Neilens <mike.neilens@gmail.com>
2021-12-02 18:03:53 +0000

    added day02 in golang

--- golang/day02/main.go
+++ golang/day02/main.go
@@ -11,41 +11,45 @@ type submarine struct {
 	aim int
 }
 
-func createListOfCommands(data string, functionCreator func(commandText string, commands []func(*submarine), ndx int, qty int)) []func(*submarine) {
+func createListOfCommands(data string, functionCreator func(commandText string, qty int) func(*submarine)) []func(*submarine) {
 	listOfWords := parser.ParseIntoListOfWords(data)
 	var result = make([]func(*submarine), len(listOfWords))
 
 	for ndx, record := range listOfWords {
 		command := record[0]
 		qty, _ := strconv.Atoi(record[1])
-		functionCreator(command, result, ndx, qty)
+		result[ndx] = functionCreator(command, qty)
 	}
 	return result
 }
 
-func part1FunctionCreator(command string, listOfCommands []func(*submarine), ndx int, qty int) {
+func part1FunctionCreator(command string, qty int) func(*submarine) {
 	switch command {
 	case "forward":
-		listOfCommands[ndx] = func(sub *submarine) { sub.x += qty }
+		return func(sub *submarine) { sub.x += qty }
 	case "up":
-		listOfCommands[ndx] = func(sub *submarine) { sub.y -= qty }
+		return func(sub *submarine) { sub.y -= qty }
 	case "down":
-		listOfCommands[ndx] = func(sub *submarine) { sub.y += qty }
+		return func(sub *submarine) { sub.y += qty }
+	default:
+		return func(sub *submarine) {}
 	}
 }
 
-func part2FunctionCreator(command string, listOfCommands []func(*submarine), ndx int, qty int) {
+func part2FunctionCreator(command string, qty int) func(*submarine) {
 	switch command {
 	case "forward":
-		listOfCommands[ndx] = func(sub *submarine) { sub.x += qty; sub.y += sub.aim * qty }
+		return func(sub *submarine) { sub.x += qty; sub.y += sub.aim * qty }
 	case "up":
-		listOfCommands[ndx] = func(sub *submarine) { sub.aim -= qty }
+		return func(sub *submarine) { sub.aim -= qty }
 	case "down":
-		listOfCommands[ndx] = func(sub *submarine) { sub.aim += qty }
+		return func(sub *submarine) { sub.aim += qty }
+	default:
+		return func(sub *submarine) {}
 	}
 }
 
-func day02(data string, functionCreator func(commandText string, commands []func(*submarine), ndx int, qty int)) int {
+func day02(data string, functionCreator func(commandText string, qty int) func(*submarine)) int {
 	var sub = submarine{0, 0, 0}
 
 	commands := createListOfCommands(data, functionCreator)
