ec24e8ad12ea0354729beaacf82b990d99e2d5c9
Mike Neilens <mike.neilens@gmail.com>
2021-12-06 12:58:18 +0000

    day06 refactor

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 
 fun partOne(data:String, days:Int):Long {
     var fishMap = turnListOfNumbersIntoMap(data)
-    (0 until days).forEach {
+    (0 until days).forEach { _ ->
         val newFishAges = listOf(1,2,3,4,5,6,7,8,0).mapIndexed {i,v ->Pair(i,fishMap[v])}.toMap().toMutableMap()
-        newFishAges[6] = newFishesAgedSix(fishMap)
+        newFishAges[6] = newFishAgedSix(fishMap)
         fishMap = newFishAges
     }
-    return fishMap.values.filter{it!=null}.sumOf{it!!}
+    return fishMap.values.filterNotNull().sumOf{it}
 }
 
-fun newFishesAgedSix(fishMap: Map<Int, Long?>) =
+fun newFishAgedSix(fishMap: Map<Int, Long?>) =
     if (fishMap[7] == null && fishMap[0] == null) null
     else if (fishMap[7] != null && fishMap[0] != null) fishMap[7]!! + fishMap[0]!!
     else if (fishMap[7] != null) fishMap[7]!!
