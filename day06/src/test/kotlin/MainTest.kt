ff040a2bb859c4f3e66fc8c8519a3e829d25f7ab
Mike Neilens <mike.neilens@gmail.com>
2021-12-06 18:05:06 +0000

    day06 refactor to add a few more tests

--- day06/src/test/kotlin/MainTest.kt
+++ day06/src/test/kotlin/MainTest.kt
@@ -4,6 +4,30 @@ import org.junit.jupiter.api.Test
 class MainTest {
     val sampleData = "3,4,3,1,2"
     val puzzleInput = "2,5,3,4,4,5,3,2,3,3,2,2,4,2,5,4,1,1,4,4,5,1,2,1,5,2,1,5,1,1,1,2,4,3,3,1,4,2,3,4,5,1,2,5,1,2,2,5,2,4,4,1,4,5,4,2,1,5,5,3,2,1,3,2,1,4,2,5,5,5,2,3,3,5,1,1,5,3,4,2,1,4,4,5,4,5,3,1,4,5,1,5,3,5,4,4,4,1,4,2,2,2,5,4,3,1,4,4,3,4,2,1,1,5,3,3,2,5,3,1,2,2,4,1,4,1,5,1,1,2,5,2,2,5,2,4,4,3,4,1,3,3,5,4,5,4,5,5,5,5,5,4,4,5,3,4,3,3,1,1,5,2,4,5,5,1,5,2,4,5,4,2,4,4,4,2,2,2,2,2,3,5,3,1,1,2,1,1,5,1,4,3,4,2,5,3,4,4,3,5,5,5,4,1,3,4,4,2,2,1,4,1,2,1,2,1,5,5,3,4,1,3,2,1,4,5,1,5,5,1,2,3,4,2,1,4,1,4,2,3,3,2,4,1,4,1,4,4,1,5,3,1,5,2,1,1,2,3,3,2,4,1,2,1,5,1,1,2,1,2,1,2,4,5,3,5,5,1,3,4,1,1,3,3,2,2,4,3,1,1,2,4,1,1,1,5,4,2,4,3"
+
+    @Test
+    fun `turning a list of values into a map`() {
+        val result = turnListOfNumbersIntoMap(sampleData)
+        assertEquals(1,result[1])
+        assertEquals(1,result[2])
+        assertEquals(2,result[3])
+        assertEquals(1,result[4])
+    }
+    @Test
+    fun `if number of fish age 7 is 2 and number of fish age 0 is 3 the total of fish that are now aged 6 is 5`(){
+        var fishForEachAge = mapOf<Int, Long?>( 7 to 2L, 0 to 3L )
+        assertEquals(5L, newFishAgedSix(fishForEachAge))
+    }
+    @Test
+    fun `if number of fish age 7 is 2 and no other fish the total of fish that are now aged 6 is 2`(){
+        var fishForEachAge = mapOf<Int, Long?>( 7 to 2L )
+        assertEquals(2L, newFishAgedSix(fishForEachAge))
+    }
+    @Test
+    fun `if number of fish age 0 is 3 and no other fish the total of fish that are now aged 6 is 2`(){
+        var fishForEachAge = mapOf<Int, Long?>( 0 to 3L )
+        assertEquals(3L, newFishAgedSix(fishForEachAge))
+    }
     @Test
     fun `part one using sample data for 80 days`() {
         assertEquals(5934,partOne(sampleData,80))
