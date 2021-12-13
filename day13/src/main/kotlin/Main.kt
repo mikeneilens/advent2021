9406c0ed2c617a40ba1ff7ccacc9fe0fa514b402
Mike Neilens <mike.neilens@gmail.com>
2021-12-13 21:33:07 +0000

    day 13 refactor

--- day13/src/main/kotlin/Main.kt
+++ day13/src/main/kotlin/Main.kt
@@ -3,29 +3,30 @@ data class Position(val x:Int, val y:Int )
 typealias Paper = Set<Position>
 typealias PaperFolder = (Paper)->Paper
 
-fun String.toPosition() =
-    Position( split(",").first().toString().toInt(),split(",").last().toString().toInt() )
 
 fun List<String>.parse() = Pair(paper(), paperFolders())
 
-private fun List<String>.paper() = filterNot{it.startsWith("fold")}.map { it.toPosition() }.toSet()
-private fun List<String>.paperFolders():List<PaperFolder> = filter{it.startsWith("fold")}.map(::getPaperFolder)
+private fun List<String>.paper() = filterNot{it.startsWith("fold")}.map (String::toPosition).toSet()
 
-fun getPaperFolder(foldInstruction:String) = {paper:Paper -> foldAtLine(paper, foldInstruction.lineToFold, foldInstruction.shouldFoldAlongY)}
+fun String.toPosition() = Position( split(",").first().toString().toInt(),split(",").last().toString().toInt() )
+
+private fun List<String>.paperFolders():List<PaperFolder> = filter{it.startsWith("fold")}.map(::makePaperFolder)
+
+fun makePaperFolder(foldInstruction:String) = { paper:Paper -> paper.foldAtLine(foldInstruction.lineToFold, foldInstruction.shouldFoldAlongY) }
 
 val String.lineToFold get() = split("=").last().toInt()
 val String.shouldFoldAlongY get() = startsWith("fold along y=")
 
-fun foldAtLine(paper:Paper, line:Int, foldOnRow:Boolean):Paper {
-    val foldedPaper = foldedPaper(paper, foldOnRow, line)
-    return foldedPositions(paper, foldOnRow, line).fold(foldedPaper){result, position -> result + position}
-}
+fun Paper.foldAtLine(line: Int, foldOnRow: Boolean):Paper = preservedPositions(this, foldOnRow, line) union foldedPositions(this, foldOnRow, line)
 
-fun foldedPaper(paper: Paper, foldOnRow: Boolean, line: Int) = if (foldOnRow) paper.filter { it.y < line }.toSet() else paper.filter { it.x < line }.toSet()
+fun preservedPositions(paper: Paper, foldOnRow: Boolean, line: Int) = if (foldOnRow) paper.beforeRow(line) else paper.beforeColumn(line)
+
+fun Paper.beforeRow(row:Int) = filter { it.y < row }.toSet()
+fun Paper.beforeColumn(column:Int) = filter { it.x < column }.toSet()
 
 fun foldedPositions(paper: Paper, foldOnRow: Boolean, line: Int) =
-    if (foldOnRow) paper.filter { it.y > line }.map { Position(it.x, line * 2 - it.y) }
-    else paper.filter { it.x > line }.map { Position(line * 2 - it.x, it.y) }
+    if (foldOnRow) paper.filter { it.y > line }.map { Position(it.x, line * 2 - it.y) }.toSet()
+    else paper.filter { it.x > line }.map { Position(line * 2 - it.x, it.y) }.toSet()
 
 fun partOne(data:List<String>):Paper {
     val (paper, paperFolders) = data.parse()
