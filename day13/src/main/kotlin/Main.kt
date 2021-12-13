7088b9d2f77361ea46d5ff06b5c18edd5adb9496
Mike Neilens <mike.neilens@gmail.com>
2021-12-13 18:06:06 +0000

    day 13 refactor

--- day13/src/main/kotlin/Main.kt
+++ day13/src/main/kotlin/Main.kt
@@ -6,15 +6,15 @@ typealias PaperFolder = (Paper)->Paper
 fun String.toPosition() =
     Position( split(",").first().toString().toInt(),split(",").last().toString().toInt() )
 
-fun List<String>.parse():Pair<Paper,List<PaperFolder>> {
-    val papers = filter { !it.startsWith("fold") }.map{it.toPosition()}.toSet()
-    val paperFolders= filter{it.startsWith("fold ")}.map(String::getPaperFolder)
-    return Pair(papers, paperFolders)
-}
+fun List<String>.parse() = Pair(paper(), paperFolders())
 
-fun String.getPaperFolder():PaperFolder  = {paper:Paper -> foldAtLine(paper, foldLine, startsWith("fold along y="))}
+private fun List<String>.paper() = filterNot{it.startsWith("fold")}.map { it.toPosition() }.toSet()
+private fun List<String>.paperFolders():List<PaperFolder> = filter{it.startsWith("fold")}.map(::getPaperFolder)
 
-val String.foldLine get() = split("=").last().toInt()
+fun getPaperFolder(foldInstruction:String) = {paper:Paper -> foldAtLine(paper, foldInstruction.lineToFold, foldInstruction.shouldFoldAlongY)}
+
+val String.lineToFold get() = split("=").last().toInt()
+val String.shouldFoldAlongY get() = startsWith("fold along y=")
 
 fun foldAtLine(paper:Paper, line:Int, foldOnRow:Boolean):Paper {
     val foldedPaper = foldedPaper(paper, foldOnRow, line)
@@ -24,10 +24,8 @@ fun foldAtLine(paper:Paper, line:Int, foldOnRow:Boolean):Paper {
 fun foldedPaper(paper: Paper, foldOnRow: Boolean, line: Int) = if (foldOnRow) paper.filter { it.y < line }.toSet() else paper.filter { it.x < line }.toSet()
 
 fun foldedPositions(paper: Paper, foldOnRow: Boolean, line: Int) =
-    if (foldOnRow)
-        paper.filter { it.y > line }.map { Position(it.x, line * 2 - it.y) }
-    else
-        paper.filter { it.x > line }.map { Position(line * 2 - it.x, it.y) }
+    if (foldOnRow) paper.filter { it.y > line }.map { Position(it.x, line * 2 - it.y) }
+    else paper.filter { it.x > line }.map { Position(line * 2 - it.x, it.y) }
 
 fun partOne(data:List<String>):Paper {
     val (paper, paperFolders) = data.parse()
