608c744cdb88f8605b7d9ed88fab2590093f5928
Mike Neilens <mike.neilens@gmail.com>
2021-12-14 19:45:57 +0000

    day 14 refactor

---
+++ day14/src/main/kotlin/Main.kt
@@ -0,0 +1,58 @@
+fun List<String>.parse() = Pair(polymerTemplate(), insertionRules())
+
+fun List<String>.polymerTemplate() = first()
+fun List<String>.insertionRules() = drop(2).map(String::polymer).toMap()
+fun String.polymer() = Pair(split(" -> ")[0], split(" -> ")[1])
+
+fun applyPolymer(polymerTemplate:List<String>, insertionRules:Map<String,String>):List<String> =
+    polymerTemplate.flatMap{listOf("${it[0]}${insertionRules[it]}","${insertionRules[it]}${it[1]}")  }
+
+fun partOne(data:List<String>):Int {
+    val (polymerTemplate, insertionRules) = data.parse()
+    var result = polymerTemplate.windowed(2,1)
+    (1..10).forEach {
+        result = applyPolymer(result, insertionRules)
+    }
+    val concatonated =   result.map{it.last()}.joinToString("")
+    val sortedResult =concatonated.groupingBy { it }.eachCount().toList().sortedBy { it.second }
+    return sortedResult.last().second - sortedResult.first().second
+}
+
+fun partTwo(data:List<String>,steps:Int):Long {
+    val (polymerTemplate, insertionRules) = data.parse()
+    var qtyPerElementPair = mutableMapOf<String,Long>()
+
+    polymerTemplate.windowed(2,1).forEach { elementPair -> qtyPerElementPair.increment(elementPair,1)   }
+
+    (0 until steps).forEach { qtyPerElementPair = qtyPerElementPair.splitAllElementPairs(insertionRules).toMutableMap() }
+
+    println(qtyPerElementPair)
+    val qtyOfEachChar = qtyPerElementPair.countChars(polymerTemplate)
+    val max = qtyOfEachChar.toList().maxOf { it.second }
+    val min = qtyOfEachChar.toList().minOf { it.second }
+    return max - min
+}
+
+fun Map<String,Long>.splitAllElementPairs(insertionRules:Map<String, String>):Map<String,Long> {
+    val updatedQtyPerElementPair = mutableMapOf<String,Long>()
+    toList().forEach { (key, value) ->
+        val (elementPair1, elementPair2) = splitElementPair(key, insertionRules)
+        updatedQtyPerElementPair.increment(elementPair1, value)
+        updatedQtyPerElementPair.increment(elementPair2, value)
+    }
+    return updatedQtyPerElementPair
+}
+
+fun splitElementPair( elementPair:String, insertionRules:Map<String, String>) =
+    Pair((elementPair[0] + insertionRules.getValue(elementPair)).toString(),
+    (insertionRules.getValue(elementPair) + elementPair[1]).toString())
+
+fun MutableMap<String,Long>.increment(key:String, num:Long) = set(key, getOrDefault(key,0) + num)
+
+fun Map<String,Long>.countChars(polymerTemplate: String):Map<Char, Long> {
+    val results = mutableMapOf<Char, Long>(polymerTemplate.last() to 1)
+    toList().forEach { (key, value) ->
+        results[key.first()] = (results[key.first()] ?: 0) + value
+    }
+    return results
+}
--- day14/src/test/kotlin/MainTest.kt
+++ day14/src/test/kotlin/MainTest.kt
@@ -1,9 +1,57 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
+val sampleData = """
+    NNCB
+
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C
+""".trimIndent().split("\n")
 class MainTest {
     @Test
-    fun `one equals one`() {
-        assertEquals(1,1)
+    fun `parse sample data`() {
+        val (polymerTemplate, pairs) = sampleData.parse()
+        assertEquals("NNCB",polymerTemplate)
+        assertEquals("B",pairs["CH"])
     }
+    @Test
+    fun `apply polymer once on sample data`() {
+        val (polymerTemplate, pairs) = sampleData.parse()
+        assertEquals(listOf("NC", "CN", "NB", "BC", "CH", "HB"), applyPolymer(polymerTemplate.windowed(2,1), pairs))
+    }
+    @Test
+    fun `part one using sample data`() {
+        assertEquals(1588, partOne(sampleData))
+    }
+    @Test
+    fun `part one using puzzle input`() {
+        assertEquals(2975, partOne(puzzleInput))
+    }
+    @Test
+    fun `part two using sample data input over 10 cycles`() {
+        assertEquals( 1588, partTwo(sampleData,10))
+    }
+    @Test
+    fun `part two using sample data input over 40 cycles`() {
+        assertEquals( 2188189693529, partTwo(sampleData,40))
+    }
+    @Test
+    fun `part two using puzzle input over 40 cycles`() {
+        assertEquals( 3015383850689, partTwo(puzzleInput,40))
+    }
+
 }
