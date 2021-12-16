4fd06443db0c29c2c04cea355cd9770b39265784
Mike Neilens <mike.neilens@gmail.com>
2021-12-16 09:15:44 +0000

    day 15 small refactor

--- day15/src/main/kotlin/Main.kt
+++ day15/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import java.util.PriorityQueue
 
-data class Position(val x:Int, val y:Int, private val maxX:Int, private val maxY:Int  ) {
-    fun surroundingPositions():List<Position> =
-        listOf(Position(x - 1, y, maxX, maxY),Position(x , y - 1, maxX, maxY),Position(x + 1, y, maxX, maxY),Position(x , y + 1, maxX, maxY))
-            .filter{ it.x in 0..maxX && it.y in 0..maxY}
+data class Position(val x:Int, val y:Int, private val max:Int  ) {
+    val surroundingPositions:List<Position> by lazy {
+        listOf(Position(x - 1, y, max),Position(x , y - 1, max),Position(x + 1, y, max),Position(x , y + 1, max))
+            .filter{ it.x in 0..max && it.y in 0..max}}
 
     override fun toString(): String = "Position($x, $y)"
 }
@@ -14,10 +14,10 @@ typealias Cavern = Map<Position, RiskLevel>
 
 fun List<String>.parse():Cavern {
     val cavern = mutableMapOf<Position, RiskLevel>()
-    val maxX = size - 1
+    val caveSize = size - 1
     forEachIndexed {y, row->
         row.forEachIndexed { x, char ->
-            cavern[Position(x,y, maxX, maxX)] = RiskLevel(char.toString().toInt())
+            cavern[Position(x,y, caveSize)] = RiskLevel(char.toString().toInt())
         }
     }
     return cavern
@@ -33,7 +33,7 @@ fun findPath2(cavern:Cavern, start:Position, target:Position):Int {
     while (stack.isNotEmpty()) {
         val (position, _) = stack.poll()
         if (position != target) {
-            position.surroundingPositions().filter { nextPosition -> cavern.isValidMove(position, nextPosition)  }.forEach {nextPosition ->
+            position.surroundingPositions.filter { nextPosition -> cavern.isValidMove(position, nextPosition)  }.forEach {nextPosition ->
                 cavern.getValue(nextPosition).cheapestCostToGetHere = cavern.getValue(position).cheapestCostToGetHere + cavern.getValue(nextPosition).num
                 stack.offer(QueueItem(nextPosition, cavern.getValue(nextPosition).cheapestCostToGetHere))
             }
@@ -49,42 +49,42 @@ fun Cavern.isValidMove(currentPosition: Position, nextPosition: Position) =
 
 fun List<String>.partOne():Int {
     val cavern = parse()
-    val maxX = size - 1
-    cavern[Position(0,0,maxX,maxX)]?.cheapestCostToGetHere = 0
-    return findPath2(cavern, Position(0,0,maxX,maxX), Position(maxX,maxX,maxX,maxX))
+    val caveSize = size - 1
+    cavern[Position(0,0,caveSize)]?.cheapestCostToGetHere = 0
+    return findPath2(cavern, Position(0,0,caveSize), Position(caveSize,caveSize,caveSize))
 }
 
 fun Cavern.makeFiveTimesBigger():Cavern {
     val result:MutableMap<Position, RiskLevel> = mutableMapOf()
-    val width = keys.maxOf { it.x } + 1
-    val newMaxX = 5 * (keys.maxOf { it.x} + 1) -1
-    forEach { position, riskLevel ->
+    val caveSize = keys.maxOf { it.x } + 1
+    val newCaveSize = 5 * (keys.maxOf { it.x} + 1) -1
+    forEach { (position, riskLevel) ->
         (0..4).forEach { row ->
             (0..4).forEach { col ->
-                result[Position(position.x + width * col, position.y + width * row  ,newMaxX,newMaxX)] = RiskLevel(calcRiskLevel(riskLevel, col, row), Int.MAX_VALUE)
+                result[Position(position.x + caveSize * col, position.y + caveSize * row  ,newCaveSize)] = calcRiskLevel(riskLevel, col, row)
             }
         }
     }
     return result
 }
 
-fun calcRiskLevel(riskLevel:RiskLevel, col:Int, row:Int):Int {
-   val newLevel = (riskLevel.num + row + col ) % 9
-   return if (newLevel == 0) 9 else newLevel
+fun calcRiskLevel(riskLevel:RiskLevel, col:Int, row:Int):RiskLevel {
+   val newNum = (riskLevel.num + row + col ) % 9
+   return if (newNum == 0) RiskLevel(9) else RiskLevel(newNum)
 }
 
 fun List<String>.partTwo():Int {
     val cavern = parse().makeFiveTimesBigger()
-    val maxX = cavern.keys.maxOf { it.x }
-    cavern[Position(0,0,maxX,maxX)]?.cheapestCostToGetHere = 0
-    return findPath2(cavern, Position(0,0,maxX,maxX), Position(maxX,maxX,maxX,maxX))
+    val caveSize = cavern.keys.maxOf { it.x }
+    cavern[Position(0,0,caveSize)]?.cheapestCostToGetHere = 0
+    return findPath2(cavern, Position(0,0,caveSize), Position(caveSize,caveSize,caveSize))
 }
 
-//This is what I started with
+/*This is what I started with
 fun findPath(cavern:Cavern, startPosition:Position, target:Position){
-    if (startPosition != target ) startPosition.surroundingPositions().filter { nextPosition -> cavern.isValidMove(startPosition, nextPosition)}.forEach { nextPosition ->
+    if (startPosition != target ) startPosition.surroundingPositions.filter { nextPosition -> cavern.isValidMove(startPosition, nextPosition)}.forEach { nextPosition ->
         cavern.getValue(nextPosition).cheapestCostToGetHere = cavern.getValue(startPosition).cheapestCostToGetHere + cavern.getValue(nextPosition).num
         findPath(cavern,nextPosition, target)
     }
 }
-
+*/
--- day15/src/test/kotlin/MainTest.kt
+++ day15/src/test/kotlin/MainTest.kt
@@ -18,17 +18,17 @@ class MainTest {
     @Test
     fun `parse sample data`() {
         val cavern = sampleData.parse()
-        assertEquals(1, cavern[Position(0,0,9,9)]?.num)
-        assertEquals(6, cavern[Position(1,3,9,9)]?.num)
-        assertEquals(7, cavern[Position(9,5,9,9)]?.num)
-        assertEquals(1, cavern[Position(9,9,9,9)]?.num)
+        assertEquals(1, cavern[Position(0,0,9)]?.num)
+        assertEquals(6, cavern[Position(1,3,9)]?.num)
+        assertEquals(7, cavern[Position(9,5,9)]?.num)
+        assertEquals(1, cavern[Position(9,9,9)]?.num)
     }
     @Test
     fun `find path using sample data`() {
         val cavern = sampleData.parse()
-        cavern[Position(0,0,9,9)]?.cheapestCostToGetHere = 0
-        val results = findPath2(cavern,Position(0,0,9,9), Position(9,9,9,9))
-        assertEquals(40, cavern[Position(9,9,9,9)]?.cheapestCostToGetHere)
+        cavern[Position(0,0,9)]?.cheapestCostToGetHere = 0
+        findPath2(cavern,Position(0,0,9), Position(9,9,9))
+        assertEquals(40, cavern[Position(9,9,9)]?.cheapestCostToGetHere)
     }
 
     @Test
@@ -45,7 +45,7 @@ class MainTest {
         val cavern = sampleData.parse()
         val bigCavern = cavern.makeFiveTimesBigger()
         assertEquals(2500, bigCavern.size)
-        assertEquals(1, bigCavern[Position(0,0,49,49)]?.num)
+        assertEquals(1, bigCavern[Position(0,0,49)]?.num)
 
         assertEquals("11637517422274862853338597396444961841755517295286", bigCavern.toList().filter{it.first.y == 0}.sortedBy { it.first.x }.map{it.second.num}.joinToString(""))
     }
